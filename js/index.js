e8ae53ea8bc062866e8022ad6f9d5df1095bbca0
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-06-03 00:56:51 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,82 @@
+// 1- misol
+// 1 dan 50 gacha bo'lgan sonlar orasida toq sonlarning kvadratlarini konsolga chigarish
+// let n = 50;
+// for(let i = 1; i<=n; i++){
+//     if(i % 2 ==1){
+//         console.log(i**2); 
+//     } 
+// }
+
+
+// 2-misol
+// 1 dan 10 gacha bo'lgan sonlarning kublarini konsolga chigarish
+// let n = 10;
+// for (let i = 1; i<=n; i++){
+//     console.log(i**3);
+// }
+
+// 3-misol
+// 1 dan 20 gacha bo'gan sonarning faktorialini konsolga chigarish
+// let n = 20;
+// let sum=1;
+// for ( let i =1; i<=n;i++){
+//     sum=sum*i;
+// }
+// console.log(sum);
+
+// 4-misol
+// 1 dan 30 gacha bo'lgan sonlarning kvadratlari yis'indisini topish
+// let sum;
+// counter=0
+// let n = 30;
+// for (let i = 1; i<=n; i++){
+//     sum=i**2;
+//     counter=counter+sum
+// }
+// console.log(counter);
+
+// 5-misol
+// 1 dan 20 gacha bo'gan sonlar orasida faqat tub sonlarni konsolga chigarish
+// let n = 20;
+
+// for (let i = 1; i<=n; i++){ 
+//     counter= 0;
+//     for(let k=1; k<=i; k++){
+//         if(i%k==0 ){
+//             counter++
+//         }
+//     }
+//     if(counter == 2){
+//         console.log(i);
+//     }
+// }
+
+// 6-misol
+// 1 dan 100 gacha bo'lgan sonlar orasida 2 ga ko'paytirilganda 100 dan kichik bo'ladigan sonlarni topish
+// let n = 100;
+// for(let i=1; i<=100;i++){
+//     if(i*2<100){
+//     console.log(i);
+//     }
+// }
+
+
+// 7-misol
+// 1 dan 100 gacha bo'lgan sonlar orasida 7 ga bo'Linadigan va 5 ga bo' Linmaydigan sonlarni topish
+// let n =100;
+// for(let i=1; i<=n;i++){
+//     if(i%7==0 && i%5!=0){
+//         console.log(i)
+//     }
+// }
+
+
+// 8-misol
+// Berilgan sonning barcha bo' luvchilarin  toping va ularning yig' indisini hisoblang
+// let n = 20;
+// let sum=0;
+// for(let i = 1;i<=n;i++){
+//     if(n%i==0){
+//         sum=sum+i
+//     }
+// }console.log(sum)   
